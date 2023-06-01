35f5636ee9903e1bf5ea26611d05a3e2dcf51900
marcocareddu <saldumaccu@tuta.io>
2023-06-01 16:18:04 +0200

    JS - Created Array & Variable

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log('JS OK')
+
+// *PHASE 1
+// Link to DOM Element
+const button = document.querySelector('button');
+const imgElement = document.querySelector('images');
+
+// Create array with images
+const images = ['./img/01.webp', './img/02.webp', './img/03.webp', './img/04.webp', './img/05.webp',];
+
+// Created image active variable
+let active = '';
