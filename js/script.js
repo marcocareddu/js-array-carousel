6d240c331695c3e280cb411bc71f1b0e57fd70c4
marcocareddu <saldumaccu@tuta.io>
2023-06-01 16:27:35 +0200

    JS - Fixed quey selector element

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ console.log('JS OK')
 // *PHASE 1
 // Link to DOM Element
 const button = document.querySelector('button');
-const imgElement = document.querySelector('images');
+const imgElement = document.querySelector('.images');
 
 // Create array with images
 const images = ['./img/01.webp', './img/02.webp', './img/03.webp', './img/04.webp', './img/05.webp',];
